12e8b9175b3c88699d55d57fe77f3a84279e4d6c
filbertalexander <71800918+filbertalexander@users.noreply.github.com>
2021-02-05 22:52:36 +0800

    Add files via upload

---
+++ prak2/strdata.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+    $varA[0] = "Albertus";
+    $varA[1] = "Filbert";
+    $varA[2] = "Charlos"
+
+     ?>
+
+    <h1>
+      <?= $varA[1] ?>
+    </h1>
+
+  </body>
+</html>
